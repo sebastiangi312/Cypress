ee016594ecb2a1e9ede1b40c9127881b6942efed
sebastiangi312 <40746232+sebastiangi312@users.noreply.github.com>
2022-04-12 20:19:05 -0500

    Finished ESlint+Configuration

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    plugins: [
+        '@typescript-eslint',
+        "cypress"
+      ],
+      "rules": {
+        "quotes": ["error", "double"],
+        "cypress/no-assigning-return-values": "error",
+        "cypress/no-unnecessary-waiting": "error",
+        "cypress/assertion-before-screenshot": "warn",
+        "cypress/no-force": "warn"
+      }
+    
+}
